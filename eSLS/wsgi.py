3286a7c9ebb6baf761122203bffee2518dc37bd2
Aubri Bowman <aubritbowman@gmail.com>
2024-03-04 11:15:25 -0600

    Renamed to eSLS

--- eSLS/wsgi.py
+++ eSLS/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for eLMS project.
+WSGI config for eSLS project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eLMS.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'eSLS.settings')
 
 application = get_wsgi_application()
